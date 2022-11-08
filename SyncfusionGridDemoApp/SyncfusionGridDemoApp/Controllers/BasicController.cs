5797608d928734607957346cb4e9288bd78f036d
Mohammad Arfizur Rahman <arahman@numberskills.se>
2022-11-08 23:52:11 +0600

    Adding js and css references

---
+++ SyncfusionGridDemoApp/SyncfusionGridDemoApp/Controllers/BasicController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SyncfusionGridDemoApp.Controllers
+{
+    public class BasicController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
